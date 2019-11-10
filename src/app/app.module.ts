beef11cc84c40c20500512960bfecbbaf9c06d7d
amnicaaaa <amna.subic@stu.ssst.edu.ba>
2019-11-10 16:50:14 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './layout/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      HeaderComponent,
+      HomeComponent,
+      LayoutComponent,
+      FooterComponent,
+      WelcomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      CommonModule,
+      NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
